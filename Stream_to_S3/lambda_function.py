9bad85ed43aeb8e9b7584ce28e6cb4e17c3bc813
Donor <donorfelita@msn.com>
2023-03-06 18:00:13 +0100

    Add Stream to S3 pipeline

---
+++ Stream_to_S3/lambda_function.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+
+import base64
+import json
+import boto3
+from datetime import datetime
+
+s3_client = boto3.client('s3')
+
+dateTimeObj = datetime.now()
+
+timestampStr = dateTimeObj.strftime("%d-%b-%Y-%H%M%S")
+
+kinesisRecords = []
+
+
+def lambda_handler(event, context):
+    # print("Received event: " + json.dumps(event, indent=2))
+
+    for record in event['Records']:
+        # Kinesis data is base64 encoded so decode here
+        payload = base64.b64decode(record['kinesis']['data']).decode('utf-8')
+
+        kinesisRecords.append(payload)
+        # this is just for logging
+        # print("Decoded payload: " + payload)
+
+    ex_string = "\n".join(kinesisRecords)
+
+    mykey = 'output-' + timestampStr + '.txt'
+
+    response = s3_client.put_object(Body=ex_string, Bucket='e-commerce-transactions', Key=mykey)
+
+    return 'Successfully processed {} records.'.format(len(event['Records']))
