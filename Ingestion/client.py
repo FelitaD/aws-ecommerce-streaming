b9613ba1c6b93ba08154774bb0655ebfe39a1bcf
Donor <donorfelita@msn.com>
2023-03-06 14:48:34 +0100

    Write client

--- Ingestion/client.py
+++ Ingestion/client.py
@@ -1,5 +1,11 @@
-import pandas
+import pandas as pd
 import requests
 
-print('hello world')
+data = pd.read_csv('data.csv', sep=',')
+api_endpoint = 'https://amhxxkxau1.execute-api.eu-west-3.amazonaws.com/beta/test'
 
+# Simulate streaming by sending every transaction (row) to API gateway
+for i in data.index:
+    transaction = data.loc[i].to_json()
+    response = requests.post(api_endpoint, data=transaction)
+    print(response)
